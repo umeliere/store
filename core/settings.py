3dfc4d294377f617e2ccbd408fda9d9edb89a16c
umeliere <umeliere@yandex.ru>
2023-07-13 16:00:30 +0300

    the middleware to protect the admin panel from users has been added

--- core/settings.py
+++ core/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'core.middleware.hide_admin.RestrictStaffToAdminMiddleware',
 ]
 
 
